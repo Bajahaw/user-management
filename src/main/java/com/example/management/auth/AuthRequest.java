d0a827c6d5c43091106ec1ee76dd77c69d7af756
Bajahaw <rathi7137@gmail.com>
2024-10-25 19:38:58 +0200

    added: validation by password length

--- src/main/java/com/example/management/auth/AuthRequest.java
+++ src/main/java/com/example/management/auth/AuthRequest.java
@@ -2,11 +2,16 @@ package com.example.management.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
-public record AuthRequest (@Email(message = "Invalid email")
-                           @NotBlank(message = "Empty email")
-                           String username,
-                           @NotBlank(message = "Empty Password")
-                           String password) {}
+public record AuthRequest (
+        @Email(message = "Invalid email")
+        @NotBlank(message = "Empty email")
+        String username,
+
+        @NotBlank(message = "Empty Password")
+        @Size(min = 8, max = 50, message = "Invalid password length")
+        String password
+) {}
--- src/main/java/com/example/management/auth/RegisterRequest.java
+++ src/main/java/com/example/management/auth/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.example.management.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
@@ -12,5 +13,6 @@ public record RegisterRequest(
         @Email(message = "Invalid email")
         String username,
         @NotBlank(message = "Empty password")
+        @Size(min = 8, max = 50, message = "Invalid password length")
         String password
 ) {}
