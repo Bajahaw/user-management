068d21c4d755675bc93f9e7f41bd986f4792b0ff
Bajahaw <rathi7137@gmail.com>
2024-11-30 22:02:14 +0100

    added: new custom exception & status code fixed

--- src/main/java/com/example/management/exceptions/UserDoesNotExistException.java
+++ src/main/java/com/example/management/exceptions/UserDoesNotExistException.java
@@ -1,7 +1,10 @@
 package com.example.management.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserDoesNotExistException extends UsernameNotFoundException {
     public UserDoesNotExistException(String message) {
         super(message);
--- src/main/java/com/example/management/exceptions/UsernameIsTaken.java
+++ src/main/java/com/example/management/exceptions/UsernameIsTaken.java
@@ -1,9 +1,10 @@
 package com.example.management.exceptions;
 
 import jakarta.validation.ValidationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = org.springframework.http.HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UsernameIsTaken extends ValidationException {
     public UsernameIsTaken(String message) {
         super(message);
