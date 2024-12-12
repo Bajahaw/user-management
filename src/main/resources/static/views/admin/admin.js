2221dcdf647fb42a5a26821e3bec519dea1235b7
Bajahaw <rathi7137@gmail.com>
2024-12-12 22:51:24 +0100

    added: modal to view users

--- src/main/resources/static/views/admin/admin.js
+++ src/main/resources/static/views/admin/admin.js
@@ -1,9 +1,12 @@
+let users = [];
 const userList = document.getElementById('userList');
 const searchInput = document.getElementById('searchInput');
 const userCount = document.getElementById('userCount');
 const addUserBtn = document.getElementById('addUserBtn');
 const addUserForm = document.getElementById('addUserForm');
 const addUserModal = new bootstrap.Modal(document.getElementById('addUserModal'));
+const userDataModal = new bootstrap.Modal(document.getElementById('userDataModal'));
+
 
 searchInput.addEventListener('input', (e) => {
     const searchTerm = e.target.value.toLowerCase();
@@ -63,7 +66,7 @@ function addUserToDashboard(user) {
     document.getElementById('userList').innerHTML +=
     `
         <li class="list-group-item list-group-item-action user-list-item d-flex justify-content-between align-items-center">
-            <div class="d-flex align-items-center">
+            <div class="d-flex align-items-center" onclick="showUserData(this)">
                 <i class="bi bi-person-circle me-3 text-secondary"></i>
                 <span class="w-75">${user.username}</span>
             </div>
@@ -75,13 +78,25 @@ function addUserToDashboard(user) {
     updateUserCount();
 }
 
+function showUserData(userElement){
+    const username = userElement.getElementsByTagName('span')[0].textContent;
+    const user = users.filter(user => user.username === username)[0];
+    console.log(user.username);
+    const body = document.querySelector('#userDataModal .modal-body');
+
+    body.children[1].innerText = user.username;
+    body.children[2].innerText = user.name;
+
+    userDataModal.show();
+}
+
 function updateUserCount() {
     const users = userList.querySelectorAll('.user-list-item');
     userCount.textContent = users.length;
 }
 
 async function fetchUsers() {
-    let users = await fetch(
+    users = await fetch(
         '/dashboard/users', {
             method: 'GET',
             headers: {
